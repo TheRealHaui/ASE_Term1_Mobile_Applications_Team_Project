2f77ef58bc82681c574133e1c0829616116ae6e1
Michael Kronberger <michael.kronberger@gmx.at>
2014-01-18 16:51:07 +0100

    BLA

--- app/models/Person.js
+++ app/models/Person.js
@@ -1,34 +1,30 @@
 exports.definition = {
-	
-/* Example response from webservice.
- * 
- * [{"address":"Allestrasse 12, 8010 Graz, Austria","emailAddress":"h.s@bekiffter.org","firstname":"Hans","id":2,"lastname":"Söllner","telephonNumber":"113 21423","version":0}]
- * 
- */	
-	
-	config: {
-		columns: {
-		    "id": "decimal",
-		    "firstname": "string",
-		    "lastname": "string",
-		    "address": "string",
-		    "emailAddress": "string",
-		    "telephonNumber": "string"		    		    		    
-		},
-		
-	},
-	extendModel: function(Model) {
-		_.extend(Model.prototype, {
-			// extended functions and properties go here
-		});
+     config: {
+                columns: {
+                    "id": "decimal",
+                    "firstname": "string",
+                    "lastname": "string",
+                    "address": "string",
+                    "emailAddress": "string",
+                    "telephonNumber": "string"                                                            
+                },
+                "adapter": {
+                    "type": "properties", 
+                    "collection_name": "persons"
+        }
+        },
+    extendModel: function(Model) {
+        _.extend(Model.prototype, {
+            // extended functions and properties go here
+        });
 
-		return Model;
-	},
-	extendCollection: function(Collection) {
-		_.extend(Collection.prototype, {
-			// extended functions and properties go here
-		});
+        return Model;
+    },
+    extendCollection: function(Collection) {
+        _.extend(Collection.prototype, {
+            // extended functions and properties go here
+        });
 
-		return Collection;
-	}
+        return Collection;
+    }
 };
