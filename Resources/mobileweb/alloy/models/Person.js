ee91433a9c495d5e4d3a92cc808671706b3d3b33
Bernhard <bernhard.eibegger.itm10@fh-joanneum.at>
2014-01-19 01:42:22 +0100

    zur View additionalinformation die Labels hinzugefuegt

--- Resources/mobileweb/alloy/models/Person.js
+++ Resources/mobileweb/alloy/models/Person.js
@@ -1,18 +1,16 @@
 exports.definition = {
     config: {
         columns: {
-<<<<<<< HEAD
-            sign: "string",
-            stockName: "string",
-            currentPrice: "decimal"
-=======
             id: "decimal",
             firstname: "string",
             lastname: "string",
             address: "string",
             emailAddress: "string",
             telephonNumber: "string"
->>>>>>> b7fc47ab5e47c8c00fe77bca7c793bf9f4659821
+        },
+        adapter: {
+            type: "properties",
+            collection_name: "persons"
         }
     },
     extendModel: function(Model) {
--- app/controllers/additionalinformation.js
+++ app/controllers/additionalinformation.js
@@ -1,1 +1,14 @@
 var args = arguments[0] || {};
+
+/* no close button for iOS as described in   http://docs.appcelerator.com/titanium/3.0/#!/guide/Alloy_Controllers
+ * because iOS is not a targed
+ */
+
+// initialize the labels
+$.lblFirstname.text = args.firstname || '';
+$.lblLastname.text = args.lastname || '';
+$.lblAddress.text = args.address || '';
+$.lblEmail.text = args.emailAddress || '';
+$.lblTelefonNo.text = args.telephonNumber || '';
+
+
