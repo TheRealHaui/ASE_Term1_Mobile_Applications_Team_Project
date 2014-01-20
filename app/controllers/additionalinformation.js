f0ff1bc71f05096d80aef6bd95fa81d729326116
Bernhard <bernhard.eibegger.itm10@fh-joanneum.at>
2014-01-20 22:32:27 +0100

    Aufloesung der Adresse hinzugefuegt

--- app/controllers/additionalinformation.js
+++ app/controllers/additionalinformation.js
@@ -5,31 +5,58 @@ var args = arguments[0] || {};
  */
 
 // initialize the labels
-$.lblFirstname.text = args.get("firstname") || '';
-$.lblLastname.text = args.get("lastname") || '';
+$.lblFirstname.text = args.get("firstName") || '';
+$.lblLastname.text = args.get("lastName") || '';
 $.lblAddress.text = args.get("address") || '';
 $.lblEmail.text = args.get("emailAddress") || '';
 $.lblTelefonNo.text = args.get("telephonNumber") || '';
 
-var MapModule = require('ti.map');
 
-var personlatitude = 47.213243;
-var personlongitude = 14.830806;
-
-var personlocation = MapModule.createAnnotation({ 
-    latitude: personlatitude,
-    longitude: personlongitude,
-    pincolor: MapModule.ANNOTATION_RED,  
-    title: args.get("lastname")
+Titanium.Geolocation.forwardGeocoder(args.get("address"),function(evt){
+	var MapModule = require('ti.map');
+	var personlatitude = 0.0;
+	var personlongitude = 0.0;
+	
+	if(evt.latitude == null){
+		personlatitude = 47.213243;	
+	}
+	else{
+		personlatitude = evt.latitude;	
+	}
+	
+	if(evt.longitude == null){
+		personlongitude = 14.830806;	
+	}
+	else{
+		personlongitude = evt.longitude;
+	}
+	
+	var personlocation = MapModule.createAnnotation({ 
+	    latitude: personlatitude,
+	    longitude: personlongitude,
+	    pincolor: MapModule.ANNOTATION_RED,  
+	    title: args.get("lastName")
+	});	
+	
+	var mv = MapModule.createView({
+	    mapType: MapModule.NORMAL_TYPE,
+	    region: {latitude:personlatitude, longitude:personlongitude, latitudeDelta:0.02, longitudeDelta:0.02},
+	    animate:true,
+	    regionFit:true,
+	    userLocation:true,
+	    annotations:[personlocation]
+	});
+	
+	$.mapview.add(mv);		
 });
 
-var mv = MapModule.createView({
-    mapType: MapModule.NORMAL_TYPE,
-    region: {latitude:personlatitude, longitude:personlongitude, latitudeDelta:0.02, longitudeDelta:0.02},
-    animate:true,
-    regionFit:true,
-    userLocation:true,
-    annotations:[personlocation]
-});
+/*
+personlocation = MapModule.createAnnotation({ 
+	    latitude: personlatitude,
+	    longitude: personlongitude,
+	    pincolor: MapModule.ANNOTATION_GREEN,  
+	    title: args.get("lastName")
+	});
 
-$.mapview.add(mv);
+*/
+
